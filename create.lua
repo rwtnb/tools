02d5504d79414678dc75588bb816f5ef10552ec3
Rodrigo S. Watanabe <rodrigo@wtnb.io>
2021-03-21 23:29:22 -0300

    removed tags arg from create command

--- create.lua
+++ create.lua
@@ -5,7 +5,7 @@ id: %s
 date: %s
 title: %s
 tags:
-- %s
+- tag
 ---
 # Heading]]
 
@@ -14,13 +14,12 @@ local function handler(parsed, command, app)
   local date = os.date('%Y-%m-%d')
   local tmp_path = os.tmpname()
   local path = ('%s/%s.md'):format(NOTES_FOLDER, id)
-  local tags = table.join(parsed.tags, "\n- ")
-  io.writefile(tmp_path, NOTE_TPL:format(id, date, parsed.title, tags))
+  io.writefile(tmp_path, NOTE_TPL:format(id, date, parsed.args:join(' ')))
   os.execute(('cat %s | nvim - +"f %s"'):format(tmp_path, path))
   os.remove(tmp_path)
 end
 
 return function(app)
-  app:command('create <title> [tags...]', 'create a note')
+  app:command('create [args...]', 'create a note')
     :action(handler)
 end
